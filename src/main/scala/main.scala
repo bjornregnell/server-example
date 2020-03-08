c53ac59d9574fd31924ee9706e1325c0620c0b10
bjornregnell <bjorn.regnell@cs.lth.se>
2020-03-08 18:34:23 +0100

    add simple hello my ip route

---
+++ src/main/scala/main.scala
@@ -0,0 +1,17 @@
+def ServerPort = 9000
+def ServerIP   = "0.0.0.0"
+def now = new java.util.Date
+
+@main def run() =
+  MyServer.start(port = ServerPort, host = ServerIP)
+
+object MyServer extends WebServer:
+  def log(msg: String): Unit = println(s"MyServer @ $now: $msg")
+
+  override def routes =
+    import akka.http.scaladsl.server.Directives._
+    import akka.http.scaladsl.server.StandardRoute
+
+    path("hello") { extractClientIP { ip =>
+      get { log(s"/hello"); reply(html.helloPage(msg = s"hello $ip")) } 
+    } }
